25c49e252ae9196089897fe36c421d3677f133a3
turbopope <PapstDonB@Googlemail.com>
2019-05-26 10:30:13 +0545

    Remove collection module

--- src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CollectionFooterComponent } from './collection-footer/collection-footer';
-import { IonicModule } from 'ionic-angular';
-@NgModule({
-	declarations: [CollectionFooterComponent],
-	imports: [IonicModule],
-	exports: [CollectionFooterComponent]
-})
-export class ComponentsModule {}
