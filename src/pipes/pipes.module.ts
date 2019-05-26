bdafadae0f0a524c2f1519e63e6cb79bdac95278
turbopope <PapstDonB@Googlemail.com>
2019-05-26 10:35:56 +0545

    Delete pipes module

--- src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { KeyValuePipe } from '../app/key-value';
-@NgModule({
-	declarations: [KeyValuePipe],
-	imports: [],
-	exports: [KeyValuePipe]
-})
-export class PipesModule {}
