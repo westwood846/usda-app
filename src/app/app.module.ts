dbf3363c50658d42c13279c630c1accfc8b6f82e
turbopope <PapstDonB@Googlemail.com>
2019-05-26 17:15:45 +0545

    Remove obsolete keyvalue pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { KeyValuePipe } from './key-value.pipe';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, KeyValuePipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
