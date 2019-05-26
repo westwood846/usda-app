eff070aa5fd55d5864c6e8650cfd29a22d4af4e2
turbopope <PapstDonB@Googlemail.com>
2019-05-26 22:26:11 +0545

    Generate collection page

---
+++ src/app/collection/collection.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CollectionPage } from './collection.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CollectionPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [CollectionPage]
+})
+export class CollectionPageModule {}
