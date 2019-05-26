1052f9e264ad9f523931a35ad5e4224efce0f5ea
turbopope <PapstDonB@Googlemail.com>
2019-05-26 21:12:28 +0545

    Move existing collection logic into collection service

--- src/app/collection.service.ts
+++ src/app/collection.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+import { uniq } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionService {
 
-  constructor() { }
+  constructor(private storage: Storage) { }
+
+  addToCollection(id: string, amount: number) {
+    this.storage.get('collection').then(collection => {
+      this.storage.set('collection', uniq([...(collection||[]), {id, amount}]));
+    })
+  }
+
+  logCollection() {
+    this.storage.get('collection').then(console.dir);
+  }
+
+  clearCollection() {
+    this.storage.set('collection', []);
+  }
 }
--- src/app/item-detail/item-detail.page.ts
+++ src/app/item-detail/item-detail.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Storage } from '@ionic/Storage';
 
 import { UsdaService } from '../usda.service';
 import { ReferenceService } from '../reference.service';
@@ -8,6 +7,7 @@ import { map } from 'rxjs/operators'
 
 import { groupBy, compact, uniq } from 'lodash';
 import { Router } from '@angular/router';
+import { CollectionService } from '../collection.service';
 
 @Component({
   selector: 'page-item-detail',
@@ -23,7 +23,7 @@ export class ItemDetailPage {
   ref = {};
 
 
-  constructor(public usda: UsdaService, private storage: Storage, private router: Router) {
+  constructor(public usda: UsdaService, private router: Router, private collectionService: CollectionService) {
     this.id = '09326'; // TODO: Get from path params
     usda.get(this.id).pipe(map(result => result['foods'][0].food)).subscribe(food => {
       console.dir(food);
@@ -40,17 +40,15 @@ export class ItemDetailPage {
   }
 
   addToCollection() {
-    this.storage.get('collection').then(collection => {
-      this.storage.set('collection', uniq([...(collection||[]), {id: this.id, amount: this.amount}]));
-    })
+    this.collectionService.addToCollection(this.id, this.amount);
   }
 
   logCollection() {
-    this.storage.get('collection').then(console.dir);
+    this.collectionService.logCollection();
   }
 
   clearCollection() {
-    this.storage.set('collection', []);
+    this.collectionService.clearCollection();
   }
 
 }
