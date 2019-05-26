b9d683f6e9063c66e43d1f2953de93c03a17eddb
turbopope <PapstDonB@Googlemail.com>
2019-05-26 10:47:25 +0545

    Update app files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
+  { path: 'detail', loadChildren: './item-detail/item-detail.module#ItemDetailPageModule' },
+  { path: 'collection', loadChildren: './item-master/item-master.module#ItemMasterPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
